d4739ba3e095ed5d6d138e6923babfad5ab2029a
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-09-27 14:29:00 -0400

    Added text

--- DisplayShapesCallie/main.lua
+++ DisplayShapesCallie/main.lua
@@ -1,6 +1,18 @@
+--------------------------------------------------------------------------------------------------
 -- Title: Display shapes
 -- Name: Callie McWaters
---
------------------------------------------------------------------------------------------
+-- Course: ICS20
+-- This program displays shapes and has their names under them
+--------------------------------------------------------------------------------------------------
 
--- Your code here
+-- create my local variable
+local halfW = display.contentWidth * 0.5
+local halfH = display.contentHeight * 0.5
+ 
+local vertices = { }
+ 
+local o = display.newPolygon( halfW, halfH, vertices )
+o.fill = { type="image", filename="mountains.png" }
+o.strokeWidth = 10
+o:setStrokeColor( 1, 5, 0 )
+
