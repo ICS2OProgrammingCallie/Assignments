b28c299a6f3d01bca0eadeea57a42ac7cfbde5da
Callie McWaters <callie.mcwaters@ocsbstudent.ca>
2018-10-11 17:52:05 -0400

    Added code

--- AnimatingImagesCallie/main.lua
+++ AnimatingImagesCallie/main.lua
@@ -8,9 +8,6 @@
 -- hide the status bar
 display.setStatusBar(display.hiddenStatusBar)
 
--- global variables
-scrollSpeed = 1
-
 -- background image with width and height
 local backgroundImage = display.newImageRect("Images/Rainbow.JPG", 2048, 1536)
 
@@ -21,11 +18,11 @@ local earthPony = display.newImageRect("Images/earthPony.png", 200, 200)
 
 -- set initial x and y positions
 pegasus.y = display.contentHeight/9
-unicorn.x = 0
+pegasus.x = 0
 unicorn.y = display.contentHeight/2
 unicorn.x = 0
-earthPony.y = display.contentHeight/1
-earthPony.x = 0
+earthPony.x = 500
+earthPony.y = display.contentHeight/9
 
 transition.to(pegasus, {x=2048, y=768, time=4000})
 
@@ -33,3 +30,20 @@ transition.to( unicorn, { rotation = unicorn.rotation-360, time=4000, onComplete
  
 transition.to(unicorn, {x=1024, y=384, time=4000})
 
+-- global variables
+scrollSpeedPony = 2
+
+-- set earthPony to be transperent
+earthPony.alpha = 0
+
+-- Function: MovePony
+-- Input: this function adds the scroll speed to the y-value of the pony
+local function MovePony(event)
+	-- adds the scroll speed to the y-value of the pony
+	earthPony.y = earthPony.y + scrollSpeedPony
+	-- chaneg the transperency of the pony every time it moves so that it fades out
+	earthPony.alpha = earthPony.alpha + 0.01
+end
+
+-- MovePony will be called over and over again
+Runtime:addEventListener("enterFrame", MovePony)
