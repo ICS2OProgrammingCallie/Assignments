66a7344778a2880380a535e6590240654a528d43
Callie McWaters <callie.mcwaters@ocsbstudent.ca>
2018-10-10 20:31:00 -0400

    Addede background

--- AnimatingImagesCallie/main.lua
+++ AnimatingImagesCallie/main.lua
@@ -1,7 +1,15 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: Animating Images
+-- Name: Callie McWaters
+-- Course: ICS20
+-- This program displays three images the move around the screen
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.hiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect ("Images/Rainbow.JPG", 2048, 1536)
