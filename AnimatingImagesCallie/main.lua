cc904e46a00c502bf18d50b6b4e1a8e93866a22a
Callie McWaters <callie.mcwaters@ocsbstudent.ca>
2018-10-10 21:10:10 -0400

    Added images

--- AnimatingImagesCallie/main.lua
+++ AnimatingImagesCallie/main.lua
@@ -12,4 +12,14 @@ display.setStatusBar(display.hiddenStatusBar)
 scrollSpeed = 3
 
 -- background image with width and height
-local backgroundImage = display.newImageRect ("Images/Rainbow.JPG", 2048, 1536)
+local backgroundImage = display.newImageRect("Images/Rainbow.JPG", 2048, 1536)
+
+-- character image with width and height
+local unicorn = display.newImageRect("Images/unicorn.png", 200, 200) 
+local pegasus = display.newImageRect("Images/pegasus.png", 200, 200)
+
+-- set the intial x and y positions
+unicorn.x = 100
+unicorn.y = display.contentHeight/3
+pegasus.x = 100
+pegasus.y = display.contentHeight/2
