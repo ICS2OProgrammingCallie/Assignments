8148c48433a91d6d1ea55c2c064283cc26bbe458
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-10-11 15:26:29 -0400

    added images

--- AnimatingImagesCallie/main.lua
+++ AnimatingImagesCallie/main.lua
@@ -9,7 +9,29 @@
 display.setStatusBar(display.hiddenStatusBar)
 
 -- global variables
-scrollSpeed = 3
+scrollSpeed = 1
 
 -- background image with width and height
 local backgroundImage = display.newImageRect ("Images/Rainbow.JPG", 2048, 1536)
+local unicorn = display.newImageRect ("Images/Unicorn.png", 200, 200)
+local pegasus = display.newImageRect ("Images/pegasus.png", 200, 200)
+
+-- set initial x and y positions
+pegasus.y = display.contentHeight/9
+unicorn.x = 0
+unicorn.y = display.contentHeight/2
+unicorn.x = 0
+
+-- Function: MovePegasus
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function
+local function MovePegasus(event)
+	-- add the scroll speed to the x value of the pegasus
+	pegasus.x = pegasus.x + scrollSpeed
+	-- make the pegasus spin
+	pegasus.x = pegasus.x + pegasus.y + 0.01
+end
+
+--MovePegasus will be called over and over again
+Runtime:addEventListener("enterFrame", MovePegasus)
