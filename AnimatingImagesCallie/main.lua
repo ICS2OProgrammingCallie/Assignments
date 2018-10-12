30fc235b002e35e90ff57cb4f061f4c4bb672c5d
Callie McWaters <callie.mcwaters@ocsbstudent.ca>
2018-10-11 21:56:10 -0400

    Finished

--- AnimatingImagesCallie/main.lua
+++ AnimatingImagesCallie/main.lua
@@ -24,35 +24,10 @@ unicorn.x = 0
 earthPony.x = 500
 earthPony.y = display.contentHeight/9
 
-transition.to(pegasus, {x=2048, y=768, time=4000})
-
-transition.to( unicorn, { rotation = unicorn.rotation-360, time=4000, onComplete=spinImage})
- 
-transition.to(unicorn, {x=1024, y=384, time=4000})
-
--- global variables
-scrollSpeedPony = 2
-scrollSpeedPegasus = 3
-
--- set earthPony to be transperent
-earthPony.alpha = 0
-
--- Function: MovePony
--- Input: this function adds the scroll speed to the y-value of the pony
-local function MovePony(event)
-	-- adds the scroll speed to the y-value of the pony
-	earthPony.y = earthPony.y + scrollSpeedPony
-	-- chaneg the transperency of the pony every time it moves so that it fades out
-	earthPony.alpha = earthPony.alpha + 0.01
-end
-
--- MovePony will be called over and over again
-Runtime:addEventListener("enterFrame", MovePony)
-
-pegasus:scale(1,1*2)
-
--- Function: MovePony
--- Input: this function makes the pegasus grow 
+-- Function: ScalePegasus
+-- Input: this function accepts an event listener
+-- Output: None
+-- Description: This function makes the pegasus grow
 local function ScalePegasus(event)
 	-- make the image grow
 	pegasus:scale(1.01,1.01)
@@ -61,8 +36,50 @@ end
 -- ScalePegasus will be called over and over again
 Runtime:addEventListener("enterFrame", ScalePegasus)
 
+-- the pegasus will move
+transition.to(pegasus, {x=2048, y=768, time=4000})
+
+-- the unicorn will move and rotate
+transition.to( unicorn, { rotation = unicorn.rotation-360, time=4000, onComplete=spinImage})
+transition.to(unicorn, {x=1024, y=384, time=4000})
+
+-- global variables
+scrollSpeedPony = 2
+
+-- set earthPony to be transperent
+earthPony.alpha = 0
+
+-- Function: MovePony
+-- Input: this function excepts a event listener
+-- Output: None
+-- Description: This function changes the transperency of the pony so it fades in
+local function MovePony(event)
+	-- adds the scroll speed to the y-value of the pony
+	earthPony.y = earthPony.y + scrollSpeedPony
+	-- change the transperency of the pony every time it moves so that it fades in
+	earthPony.alpha = earthPony.alpha + 0.01
+end
+	
+-- MovePony will be called over and over again
+Runtime:addEventListener("enterFrame", MovePony)
+
 -- display text on the screen
 local textObject = display.newText( "By: Callie", 500, 400, nil, 85)
 
--- set text colour
-textObject:setTextColor(155/255, 60/255, 170/255)
+-- Change the colour of the text
+textObject:setTextColor(175/255, 60/255, 180/255)
+
+-- set earthPony to be transperent
+textObject.alpha = 0
+
+-- Function: MoveText
+-- Input: this function excepts a event listener
+-- Output: None
+-- Description: This function changes the transperency of the pony so it fades in
+local function MoveText(event)
+	-- change the transperency of the text every time it moves so that it fades in
+	textObject.alpha = textObject.alpha + 0.01
+end
+
+-- MoveText will be called over and over again
+Runtime:addEventListener("enterFrame", MoveText)
