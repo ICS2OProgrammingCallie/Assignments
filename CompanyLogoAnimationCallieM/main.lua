f90d0e594853cebfce901b344849fecb6ca30b96
Callie McWaters <callie.mcwaters@ocsbstudent.ca>
2018-11-10 11:07:58 -0500

    .

--- CompanyLogoAnimationCallieM/main.lua
+++ CompanyLogoAnimationCallieM/main.lua
@@ -11,8 +11,10 @@ display.setStatusBar(display.HiddenStatusBar)
 -- set the background colour
 
 --
-local logo = display.newImageRect("Images/CompanyLogo.png", 0, 0)
-logo.width = display.contentWidth/2
-logo.height = display.contentHeight/2
-logo.x = display.contentCenterX/.5
-logo.y = display.contentCenterY/1
+local logo = display.newImageRect("Images/CompanyLogo.png", 500, 500)
+logo.x = display.contentWidth/2
+logo.y = display.contentHeight/2
+
+-- the unicorn will move and rotate
+transition.to( logo, { rotation = logo.rotation-360, time=4000, onComplete=spinImage})
+transition.to( logo, {x=1024, y=384, time=4000})
